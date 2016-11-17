04880c71c4ef6c48bd2ea786141efa0ae49a7c4d
Aaditya Menon <AadityaMenon007@gmail.com>
2016-11-18 01:10:15 +0530

    Adding more methods in BST

--- Before End Sem/src/com/binarysearchtree/BinarySearchTree.java
+++ Before End Sem/src/com/binarysearchtree/BinarySearchTree.java
@@ -9,10 +9,41 @@ public class BinarySearchTree {
 		root = null;
 	}
 	
-	public void add(int n) {
+	public Link add(Link node, int n) {
 		Link temp = new Link(n);
 		if (root == null) {
-			
+			root = temp;
+		}
+		else if (n >= root.getData()) {
+			root.setRight(add(node.getRight() , n));
+		}
+		else {
+			root.setLeft(add(node.getLeft() , n));
+		}
+		return root;
+	}
+	
+	public void preorder(Link node) {
+		if (node != null) {
+			System.out.print(node.getData() + " ");
+			preorder(node.getLeft());
+			preorder(node.getRight());
+		}
+	}
+	
+	public void postorder(Link node) {
+		if (node != null) {
+			preorder(node.getLeft());
+			preorder(node.getRight());
+			System.out.print(node.getData() + " ");
+		}
+	}
+	
+	public void inorder(Link node) {
+		if (node != null) {
+			preorder(node.getLeft());
+			System.out.print(node.getData() + " ");
+			preorder(node.getRight());
 		}
 	}
 }
